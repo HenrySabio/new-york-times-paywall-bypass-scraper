94d7c079f79cdabc9ce68077851a3fd54b604082
Henry Sabio <hsabio.a@gmail.com>
2021-01-20 19:45:11 -0500

    All relevant article data assigned to appropriate variables

--- scraper.js
+++ scraper.js
@@ -1,9 +1,45 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const testURL = "Enter url of NYT article you want to scrape";
+const testURL = "insert nyt article url here";
 
 axios.get(testURL).then((res) => {
     const $ = cheerio.load(res.data);
+
+    console.log("\n");
     console.log($("#site-content").find("h1").text());
+    const articleTitle = $("#site-content").find("h1").text();
+
+    console.log("\n");
+    console.log($("#site-content").find("#article-summary").text());
+    const articleSummary = $("#site-content").find("#article-summary").text();
+
+    console.log("\n");
+    console.log($("#site-content").find(".css-4z5zii").text())
+    const articleAuthors = $("#site-content").find(".css-4z5zii").text();
+
+
+    console.log("\n");
+    console.log($("#site-content").find(".last-byline").text());
+    const authorName = $("#site-content").find(".last-byline").text();
+
+    console.log("\n");
+    console.log($(`img[title="${authorName}"]`).attr("src"));
+    const authorPicture = $(`img[title="${authorName}"]`).attr("src");
+
+    console.log("\n");
+    console.log($("li").find("time").text());
+
+    console.log("\n");
+    const articleSections = []
+    $(`section[name = articleBody]`).find("p").each(function (i, el) {
+        articleSections[i] = $(this).html();
+    }); 
+
+    console.log(articleSections);
+
+    // console.log($(`section[name = articleBody]`).html());
+    // const articleCopy = $(`section[name = articleBody]`).html();
+    
+    
   });
